e2e78134abf0dff40d72e1184653b3feb074ef4a
Samiabarhili <barhili.samia@gmail.com>
2024-05-25 01:44:57 +0200

    Create athlete_training.h

---
+++ athlete_training.h
@@ -0,0 +1,22 @@
+#ifndef ATHLETE_TRAINING_H
+#define ATHLETE_TRAINING_H
+
+#include "datetime_util.h"
+
+typedef struct {
+    char name[50];
+} Athlete;
+
+typedef struct {
+    char date[50];
+    char type[50];
+    char time[50];
+    int position;
+} Training;
+
+void add_training(const char *filename);
+void athlete_summary(const char* filename, const char* type);
+void athlete_progression(const char* filename, const char* type, Date date1, Date date2);
+void top_athletes(Athlete *athletes, int num_ath, const char* type, int num_top);
+
+#endif
