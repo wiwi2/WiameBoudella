ba6611e1f0a5fb604116f17073e8bcc8f8d0a21e
Samiabarhili <barhili.samia@gmail.com>
2024-05-25 01:50:56 +0200

    Update file_io.h

--- file_io.h
+++ file_io.h
@@ -5,6 +5,6 @@
 
 void write_training_data(const char* filename, Training* trainings, int num_trainings);
 void get_trainings(const char *filename);
-void add_training(const char *filename);
+//void add_training(const char *filename);
 
 #endif /* FILE_IO_H */
